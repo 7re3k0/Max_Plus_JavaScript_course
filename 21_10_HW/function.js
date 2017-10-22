547bd8ac6800649bf238de8248ffd5b0b883ff25
Mark Markov <markymarkov@gmail.com>
2017-10-22 18:27:13 +0300

    Ex 2 completed

--- 21_10_HW/function.js
+++ 21_10_HW/function.js
@@ -8,3 +8,35 @@ function printName() {
 
   execPrint(yourName);
 }
+
+function converter() {
+  var ammount_input = document.getElementById('ammount').value,
+    currency_iput = document.getElementById('currency').value,
+    currency_iput = currency_iput.toUpperCase();
+
+  document.getElementById('converterOutput').innerHTML = 'Това в лева е = <span class="highlight">' + converting_to_bgn(ammount_input, currency_iput) + '</span>';
+  document.getElementById('converterOutput').style.display = 'inline';
+
+  function converting_to_bgn(ammount, currency) {
+    if (currency == 'USD') {
+      //USD to BGN 1.65538
+      return ammount * 1.65538 + ' BGN';
+
+    } else if (currency == 'EUR') {
+      // EUR to BGN 1.95583
+      return ammount * 1.95583 + ' BGN';
+
+    } else if (currency == 'GBP') {
+      // GBP to BGN 2.18273
+      return ammount * 2.18273 + ' BGN';
+
+    } else if (currency == 'CHF') {
+      //CHF to BGN 1.68184
+      return ammount * 1.68184 + ' BGN';
+
+    } else {
+      return 'Не работим с тази валута!'
+    }
+  };
+
+}
