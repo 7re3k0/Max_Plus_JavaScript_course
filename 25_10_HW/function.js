823e1f94c9362d1bed66ad75b10181337bd50b0b
Mark Markov <markymarkov@gmail.com>
2017-10-25 16:00:02 +0300

    Ex 2 to 4 completed

--- 25_10_HW/function.js
+++ 25_10_HW/function.js
@@ -4,20 +4,39 @@ function Dog (breed, size, age, color) {
                 this.age   = age;
                 this.color = color;
                 this.Eat = function() {
-                    console.log(this.name + " започна да яде.");
+                    console.log(this.name + ' започна да яде.');
                 };
                 this.Sleep = function() {
-                    console.log(this.name + " заспа.");
+                    console.log(this.name + ' заспа.');
                 };
                 this.Sit = function() {
-                    console.log(this.name + " седна!");
+                    console.log(this.name + ' седна!');
                 };
                 this.Run = function() {
-                    console.log(this.name + " се затича като вихър!");
+                    console.log(this.name + ' се затича като вихър!');
                 };
 
             };
-
+console.log('----------------Упражнение 1----------------');
+console.log(Dog);
 var mastif = new Dog('Neapolitan Mastiff', 'Large', 5, 'Black'),
 maltese    = new Dog('Maltese','Small', 2,'White'),
 chowChow   = new Dog('Chow Chow', 'Medium', 3, 'Brown');
+
+console.log('----------------Упражнение 2----------------');
+console.log([mastif,maltese,chowChow]);
+
+console.log('----------------Упражнение 3----------------');
+var jeff = Object.assign({}, chowChow);
+console.log(jeff);
+jeff.age = 10;
+jeff.color = 'Orange';
+console.log('After change:');
+console.log(jeff);
+
+console.log('----------------Упражнение 4----------------');
+jeff.name = {
+  'name1': 'Jeffry',
+  'name2': 'Jeff'
+};
+console.log('Name 1 is \"' + jeff.name.name1 + '\" and name 2 is \"' + jeff.name.name2 + '\".');
