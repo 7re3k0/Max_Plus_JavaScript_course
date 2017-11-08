376e295821ccc1e0804daa305f25b970d4c270a1
Mark Markov <markymarkov@gmail.com>
2017-11-08 15:59:34 +0200

    01.11 homework upload

--- 01_11_HW/function.js
+++ 01_11_HW/function.js
@@ -1,9 +1,48 @@
-document.getElementsByTagName('table')[0].addEventListener('click',function (e) {
-    console.log(e.target.value);
-    if (e.target.tagName ) {
+document.getElementsByTagName('table')[0].addEventListener('click', function(e) {
+  e = e || window.event;
+  var target = e.target || e.srcElement,
+  text = target.textContent || text.innerHTML;
+  var input = document.getElementById('input'),
+    clearBtn = document.getElementById('clearBtn'),
+    result = document.getElementById('result'),
+    equal = document.getElementById('equal');
 
+  if (target == input || target == result) {
+    return;
+  } else if (target == clearBtn) {
+    input.value = '';
+    return;
+  } else if (target == equal) {
+    var tempResult = input.value;
+    input.value = eval(tempResult);
+    setTimeout(function() {
+      input.value = ''
+    }, 1000);
+    return ;
+  } else {
+    input.value += text;
+    return;
+  }
+}, false);
+
+function bgChange(x) {
+  x.style.background = '#f4f5f6';
+};
+
+function bgNormal(x) {
+  x.style.background = 'white';
+}
+
+document.getElementById("input")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      var input = document.getElementById('input');
+      var tempResult = input.value;
+      input.value = eval(tempResult);
+      setTimeout(function() {
+        input.value = ''
+      }, 1000);
+      return ;
     }
-    var input = document.getElementById('input');
-    input.value += e.target.innerHTML;
-    eval(input);
 });
